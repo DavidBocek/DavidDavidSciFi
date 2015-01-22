a4bfef242099ec0149954d105f3c8df061761b5d
DavidBocek <dmbocek@gmail.com>
2015-01-22 01:06:19 -0500

    preparing for merge boiz

---
+++ SciFi/Assets/Scripts/Objects and Interactables/ImpactNoise.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImpactNoise : MonoBehaviour {
+
+	public AudioClip[] rifleNoiseFX;
+	public AudioClip[] SMGNoiseFX;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void makeNoise(Vector3 point, bool SMGMode)
+	{
+		if(SMGMode)
+			AudioSource.PlayClipAtPoint(SMGNoiseFX[Random.Range(0, SMGNoiseFX.Length)], point);
+		else
+			AudioSource.PlayClipAtPoint(rifleNoiseFX[Random.Range(0, rifleNoiseFX.Length)], point);
+
+	}
+}
